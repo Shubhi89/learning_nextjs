2b484501eb11044e9119e5961eb3e0ebace2c6a8
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-08-20 13:49:02 +0530

    adding moving button

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import React from "react";
 import { Spotlight } from "@/components/ui/Spotlight";
+import { Button } from "@/components/ui/moving-border";
 
 export default function HeroSection() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-        <Spotlight
+      <Spotlight
         className="-top-40 left-0 md:-top-20 md:left-20 lg:left-60"
         fill="white"
       />
@@ -18,9 +19,16 @@ export default function HeroSection() {
           journey today. Whether you are a beginner or looking to refine your
           skills , join us to unlock your true potential
         </p>
-        
+
         <div className="mt-5">
-          <Link href={"/courses"}>Explore Courses</Link>
+          <Link href={"/courses"} className="">
+            <Button
+              borderRadius="1.75rem"
+              className="bg-white px-2 dark:bg-black text-black dark:text-white border-neutral-200 dark:border-white/[0.2]"
+            >
+              Explore  Courses
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
