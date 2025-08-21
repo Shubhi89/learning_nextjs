4ec30103876643cff7ce78fa73a493e56fbe6902
Shubhi Dwivedi <shubhidwivedi1415@gmail.com>
2025-08-21 18:03:03 +0530

    cards section and sticky scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Courses from "@/components/courses";
 import HeroSection from "@/components/heroSection";
+import WhyChooseUs from "@/components/whyChooseUs";
 
 export default function Home() {
   return (
     <><HeroSection/>
-    <Courses/></>
+    <Courses/>
+    <WhyChooseUs/>
+    </>
   );
 }
--- src/components/courses.tsx
+++ src/components/courses.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { BackgroundGradient } from "./ui/background-gradient";
 import courseData from "@/data/music.json";
-import { Button } from "./ui/moving-border";
 import Link from "next/link";
 
+interface Course {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
+}
+
 export default function Courses() {
+  const featuredCourses = courseData.courses.filter((course : Course) => course.isFeatured);
   return (
     <div className="py-12 ">
       <div>
@@ -14,17 +24,30 @@ export default function Courses() {
         </div>
       </div>
       <div className="mt-10">
-        
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+            {featuredCourses.map((course:Course)=> {
+                return (
+                    <div key={course.id} className="flex justify-center">
+                        <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                            <div className="p-4 sm:p-6 flex flex-col items-center text center flex-grow">
+                                <p className="text-3xl">{course.title}</p>
+                                <p className="mt-5">{course.description}</p>
+                                <Link className="mt-5" href={`/courses/${course.slug}`}>
+                                Learn More</Link>
+                            </div>
+                        </BackgroundGradient>
+                    </div>
+                )
+            })}
+        </div>
       </div>
       <div>
         <div className="mt-20 text-center">
-          <Link href={"/courses"}>
-            <Button
-              borderRadius="1.75rem"
-              className="bg-white px-2 dark:bg-black text-black dark:text-white border-neutral-200 dark:border-white/[0.2]"
-            >
-              View All Courses
-            </Button>
+          <Link
+            href={"/courses"}
+            className="px-5 py-4 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
+          >
+            View All Courses
           </Link>
         </div>
       </div>
